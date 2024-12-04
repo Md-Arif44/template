1f71b4d6135173d652e6ec01e26a9a7b40ede073
Md-Arif44 <mohammadarifsb2018@gmail.com>
2024-12-05 01:05:31 +0600

    Update tem.cpp

--- tem.cpp
+++ tem.cpp
@@ -1,4 +1,22 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
 using namespace std;
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
