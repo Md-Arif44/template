f314f030905ebc27ba0faa16efdf3930372459e4
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-08-17 02:25:13 +0600

    Update and rename SSSP.cpp to SP.cpp

---
+++ SP.cpp
@@ -0,0 +1,54 @@
+
+Dijkstra
+     struct edge{
+         ll fo,w;
+         edge(ll fo,ll w):fo(fo),w(w){}
+         bool operator<(edge const &e)const {
+          return e.w<w;
+          }
+        };
+        int n,m;
+        cin>>n>>m;
+        vector<vector< edge >>g(n);
+        vector<ll>dis(n,inf);
+        int u,v;
+        ll w;
+        for(int i=0;i<m;i++){ 
+          cin>>u>>v>>w;
+          --u,--v;
+         g[u].push_back({v,w});
+       
+        }
+        priority_queue<edge>q;
+        q.emplace(0,0);
+        dis[0]=0;
+         while(!q.empty()){
+           auto v=q.top();
+           q.pop();
+           if(v.w>dis[v.fo])continue ;    
+          
+           for(auto &[u,w]:g[v.fo]){
+                if(w+dis[v.fo]<dis[u]){ 
+                  dis[u]=w+dis[v.fo];
+                  q.emplace(u,dis[u]);
+                }
+           }          
+        }
+     
+ Floyd Warshall
+  int n,m,q;
+  vector<vector<ll>>dis(n,vector<ll>(n,inf));
+  
+  for(int i=0;i<n;i++)dis[i][i]=0;
+    
+  for(int k=0;k<n;k++){
+     for(int i=0;i<n;i++){
+       for(int j=0;j<n;j++){
+          if(dis[i][k]!=inf && dis[k][j]!=inf ){
+            dis[i][j]=min(dis[i][j],dis[i][k]+dis[k][j]);
+          }
+       }
+     }
+  }
+
+
