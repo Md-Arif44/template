2de8ebe4c70ffaf911f43dcce240c35469b574a0
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-08-17 02:10:22 +0600

    Update tem.cpp

--- tem.cpp
+++ tem.cpp
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <chrono>
 #include <climits>
+#include <cstdint>
 #include <cmath>
 #include <complex>
 #include <cstring>
@@ -13,89 +14,110 @@
 #include <iostream>
 #include <map>
 #include <numeric>
+#include <stack>
 #include <queue>
 #include <random>
 #include <set>
 #include <vector>
 using namespace std;
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+ 
+ 
 #ifndef ONLINE_JUDGE 
 #include "template.cpp"
 #else
   #define debug(...) 
   #define debugArr(...)
 #endif
-using namespace __gnu_pbds;
-template<class T>using ordered_set=tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
-template<class T>using ordered_multiset=tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
-#define findc(cn, abc) ((cn).find(abc) != (cn).end())
+ 
+#define Tct   template<typename T>
+#define Tctu  template<typename T,typename U>
 #define clr(cnt, x) memset((cnt), (x), sizeof(cnt))
 typedef long long ll;
-template <class T>using V=vector<T>;
+Tct using V=vector<T>;
 #define all(x) (x).begin(), (x).end()
 #define lb lower_bound
 #define ub upper_bound
 #define bg(cn) cn.begin()
-template<typename T>
-istream &operator>>(istream &in,vector<T>&a){for(T &i:a)in>>i;return in;}
-template<typename T>ostream &operator<<(ostream &out, const vector<T> &a) {for (auto i : a) {out << i << " ";}return out;}
-template<typename T>void remDup(vector<T> &v){sort(all(v)); v.erase(unique(all(v)),end(v));}
-template<typename T,typename U>void safeErase(T &t, const U &u) {auto it = t.find(u);assert(it != end(t)); t.erase(it);}
-template<class T>int lwb(const V<T>&a,const T&b){return int(lb(all(a), b)-bg(a));}// (v[r]>=x && v[l]<x) return r;
-template<class T>int upb(const V<T>&a,const T&b){return int(ub(all(a),b)-bg(a));}//( v[r]>x && v[l]<=x) return r;
+#define Pop(cn) cn.pop_back()
+#define ff first
+#define bk(x) x.back() 
+#define fk(x) x.fornt()
+#define eb emplace_back
+#define ss second 
+#define pb push_back
+ 
+Tct void remDup(vector<T> &v){sort(all(v)); v.erase(unique(all(v)),end(v));}
+Tctu void safeErase(T &t, const U &u) {auto it = t.find(u);assert(it != end(t)); t.erase(it);}
+Tct int lwb(const V<T>&a,const T&b){return int(lb(all(a), b)-bg(a));}// (v[r]>=x && v[l]<x) return r;
+Tct int upb(const V<T>&a,const T&b){return int(ub(all(a),b)-bg(a));}//( v[r]>x && v[l]<=x) return r;
+Tctu int CC(const T&a,const U&b){return int(count(all(a),b));}
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+inline ll getrandom(ll a,ll b) { return uniform_int_distribution<ll>(a,b)(rng); }
+ 
+void YN(bool x){if(x)cout<<"YES\n";else cout<<"NO\n";}
 ll SUM_(ll n){return (n*(n+1))/2;}
 ll SUM_(ll a,ll b) { return (b- a+1) * (a + b) / 2;}
 ll A_SUM(ll n,ll a,ll b){return (n*(a+b))/2;}
 ll G_SUM(ll k,ll a,ll b){return ((b*k)-a)/(k-1);}
 ll cdiv(ll a, ll b) {return a / b + ((a ^ b) > 0 && a % b);} 
-ll fdiv(ll a, ll b) {return a / b - ((a ^ b) < 0 && a % b);} 
-const ll inf = (ll)1e18;
-const ll _inf=(ll)-1e18;  
-const int dx[4]{1, 0, -1, 0},dy[4]{0, 1, 0, -1};  
-#define ff first
+Tctu bool cmax(T &a, U b){return (a<b?a=b,1:0 );}
+Tctu bool cmin(T &a, U b){return (a>b?a=b,1:0 );}
+const ll inf = 2e18;
 #define PQ priority_queue
-template<class T>using  PQ1 =PQ<T,V<T>,greater<T>>;
-template<class T>using  PQ2 =PQ<T,V<pair<T,T>>,greater<pair<T,T>>>; 
-#define ss second 
-#define pb push_back
-#define to_str(x) (to_string(x))
-#define to_ll(x) (stoi(x))
+Tct using  PQ1 =PQ<T,V<T>,greater<T>>;
+ 
+#define to_str(x) (to_string(x)) 
+#define to_ll(x) (stoll(x))
 #define to_char(x) ((x)+'0')
 #define to_int(x) ((x)-'0')
-constexpr int Pct(int x){return __builtin_popcount(x);}  
-constexpr int Msb(int x){return x==0?0:31 -__builtin_clz(x);}  
-constexpr int P2(int x) { return 1 << x; }
-constexpr int Msk2(int x) { return P2(x) - 1;}
-constexpr int Lsb(int x) { return  x&~(x-1);}
-typedef vector<pair<ll,ll>> vll;
-typedef vector<pair<int,int>>vii;
-typedef vector<ll>vl;  
+ 
+const int dx[] = {1, -1, 0, 0, 1, 1, -1, -1};  
+const int dy[] = {0, 0, 1, -1, 1, -1, 1, -1};  
+ 
+constexpr int Pct(ll x){return __builtin_popcountll(x);}  
+constexpr int Msb(ll x){return int(log2(x));}  
+constexpr __int128_t P2(ll x) { return __int128_t(1) << x; }
+constexpr ll Msk2(ll x) { return P2(x) - 1;}
+constexpr ll Lsb(ll x) { return  x&~(x-1);}
+constexpr  bool Is_set(ll x,ll j) { return  (x&(1ll<<j) )==(1ll<<j);}
+ 
 typedef pair<int,int> pii;
-#define eb emplace_back
 typedef pair<ll, ll> pll;
-#define sz(n) (int)(n).size() 
+typedef V<pll> vll;
+typedef V<pii >vii;
+typedef V<V<int> > vvi;
+typedef vector<ll>vl;  
+typedef V<vl>vvl;
+#define sz(n) int((n).size()) 
 #define fora(cn) for(auto &x : (cn))
-using u_128 = unsigned __int128;
-using i_128=__int128_t;
+using   u_128 = unsigned __int128;
+using   i_128=__int128_t;
 #define PI 2*acos(0) 
 #define el '\n'
 typedef unsigned long long ull;
 typedef vector<int> vi;
 #define se(X) setprecision(X)
 #define rall(x) (x).rbegin(), (x).rend()
-#define rep(n) for(int (i)=(0);(i)<(n);(i)++)
-#define forr(z,x,n) for(int (z)=(x);(z)<(n);(z)++)
-#define rfor(z,x,n) for(int (z)=(x);(z)>=(n);(z)--)
-const int MX = (int)2e5 + 5;
-int main(){
-   ios::sync_with_stdio(false);cin.tie(nullptr);
-  int tt = 1;
-     cin >> tt;
-while(tt--){
-        
+#define rep(i,r) for(int (i)=(0);(i)<(r);(i)++)
+#define rep1(i,l,r) for(int (i)=(l);(i)<(r);(i)++)
+#define repr(i,r,l) for(int (i)=(r);(i)>=(l);(i)--)
+const int MX=100005;
 
-
-  }
-  return 0;
+void solve(){
+  
 }
+int main() {
+ ios::sync_with_stdio(false);
+ cout.tie(nullptr) ;
+ cin.tie(nullptr);
+ 
+ int tt=1;
+   cin>>tt;
+  while(tt--){
+    solve();
+  }
+ return 0;
+}   
+ 
+ 
+
