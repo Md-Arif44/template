698cf5ce258f00651eba60ee09b6abdcaaeb74be
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-08-17 02:18:44 +0600

    Update ex2.cpp

--- ex2.cpp
+++ ex2.cpp
@@ -1,14 +1,3 @@
-ll Bigmod ( ll a,ll p,ll m ){
-    ll res = 1ll; ll x = a;
-    while (p){
-        if (p&1)res=(res*x) % m;
- 
-        x=(x*x)%m;
-      p >>= 1ll;
-    }
-    return res;
-}
-// div
 vector<ll> Divisors(ll n) {
  vector<ll> divisors;
     for (long long i = 1; i * i <= n; ++i) {
@@ -20,56 +9,67 @@ vector<ll> Divisors(ll n) {
     }
     return divisors;
 }
-ll gcd(ll a,ll b){
-    if (a == 0ll)    return b;
-    return gcd(b % a, a);
-}
-ll lcm(ll a, ll b){
-    return (a*b) / gcd(a, b);
-} 
-//bs
+// subarray
+  vector<int> lop(n),rop(n) ,stk;  
+    rep(i,n){
+        while(sz(stk) && a[stk.back()]<a[i] ){
+             stk.pop_back() ;
+        }
+        lop[i]= (stk.empty()? -1: stk.back()) ;
+       stk.pb(i) ;
+    } 
+    stk.clear();
+    repr(i,n-1,0){
+        while(sz(stk) &&  a[stk.back()] <=a[i] ){
+             stk.pop_back() ;
+        }
+      rop[i]= (stk.empty()?n:stk.back() );
+      stk.pb(i);
+    }
+
+//bs minium  true r
   auto good=[](ll x)->bool {
 
    };
    ll l=-1,r=1;
     while(!good(r))r<<=2;
     
-    while(l+1<r){
+    while(r >l+1){
      ll mid=(l+r)>>1;
      if(good(mid))r=mid;
      else l=mid;
     }
-//Queue  
-struct Two_stack{
-            vector<ll>s,op1,op2;
-           void push(ll x){
-             s.push_back(x);
-           }           
-           ll remove(){
-             ll res=s.back();
-              s.pop_back();
-             return res;
-           }
-           pair<ll,ll> get(){
-            return {};
-           }
-           int size(){
-            return s.size();
-           }
-    };
-    Two_stack pre,suf;
-    void add(ll x){
-       suf.push(x);
+
+
+// bs maximum true l
+  auto good=[](ll x)->bool {
+
+   };
+   ll l=0,r=1;
+    while(good(r))r<<=2;
+    
+    while(r >l+1){
+     ll mid=(l+r)>>1;
+     if(good(mid))l=mid;
+     else r=mid;
     }
-    void remove(){
-        if(!pre.size()){
-             while(suf.size()){
-                 ll x=suf.remove();
-                 pre.push(x);
-             }
+
+
+    
+vvl PrefixSums2D(vvl &a) {
+  // ...1 base
+    ll n = a.size(),m = a[0].size();
+    vvl prefixSum(n + 1, vl(m + 1, 0));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            prefixSum[i + 1][j + 1] = prefixSum[i][j + 1] + prefixSum[i + 1][j] - prefixSum[i][j] + a[i][j];
         }
-        pre.remove();
     }
-    bool good(){
-      
-    }
+    return prefixSum;
+}
+ll get(int rx,int ry,int lx,int ly,vvl &pre){
+  // ... 1 base l to r inclusive
+  lx--,--ly;
+  return pre[rx][ry]+pre[lx][ly]-pre[rx][ly]-pre[lx][ry];
+
+}
