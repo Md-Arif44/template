c8c8a3ce05ca83ea5157d45716b84cebc5f46d74
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-09-02 00:16:54 +0600

    Update tem.cpp

--- tem.cpp
+++ tem.cpp
@@ -42,12 +42,17 @@ template<class T>using ordered_multiset=tree<T, null_type,less_equal<T>, rb_tree
 #include <vector>
 using namespace std;
  
- 
+
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename... Args> ostream& operator<<(ostream& os, const tuple<Args...>& t) { os << '('; apply([&os](const Args&... args) { size_t n = 0; ((os << args << (++n != sizeof...(Args) ? ", " : "")), ...); }, t); return os << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
 #ifndef ONLINE_JUDGE 
-#include "template.cpp"
+#define dbg(...) cerr << '[' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 #else
-  #define debug(...) 
-  #define debugArr(...)
+#define dbg(...)
 #endif
  
 #define Tct   template<typename T>
