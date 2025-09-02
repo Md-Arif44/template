5a63c31e8c91ceaa7d5c667240502afa901ab9a4
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-09-03 00:57:48 +0600

    Update segtree.cpp

--- segtree.cpp
+++ segtree.cpp
@@ -1,15 +1,15 @@
-
+ 
 template <class T, T (*op)(T, T), T (*e)()> 
 struct segtree {
   int n,size;
   vector<T> d;
   segtree(const vector<T>& v){
-      n=sz(v),size=1; 
+      n=int(v.size()), size=1; 
       while(size<n)size<<=1;
       
         d=vector<T>(2 * size, e());
-        rep(i,n)d[size + i] = v[i];
-        repr(i,size - 1,1) update(i);
+        for(int i=0;i<n;i++)d[size + i] = v[i];
+        for(int i=size-1;i>=1;i--) update(i);
     }
     void update(int k) { 
       d[k] = op(d[2 * k], d[2 * k + 1]); 
@@ -26,7 +26,7 @@ struct segtree {
             if (l & 1) sml = op(sml, d[l++]);
             if (r & 1) smr = op(d[--r], smr); 
         }
-        return op(sml, smr);
+      return op(sml, smr);
     }
 };
 
@@ -37,125 +37,6 @@ ll op(ll x ,ll y){
 ll e(){
    return 0ll ;
 }
-
-
-
-template <class S, S (*op)(S, S), S (*e)()> 
-struct segtree {
-  public:
-    segtree() : segtree(0) {}
-    explicit segtree(int n) : segtree(std::vector<S>(n, e())) {}
-    explicit segtree(const std::vector<S>& v) : _n(int(v.size())) {
-      if(_n&(_n-1)== 0)size=1<<Msb(_n);
-      else size=1<<(Msb(_n)+1);
-      
-        log =__builtin_ctz((unsigned int)size);
-        d = std::vector<S>(2 * size, e());
-        for (int i = 0; i < _n; i++) d[size + i] = v[i];
-        for (int i = size - 1; i >= 1; i--) {
-            update(i);
-        }
-    }
-
-    void set(int p, S x) {
-        assert(0 <= p && p < _n);
-        p += size;
-        d[p] = x;
-        for (int i = 1; i <= log; i++) update(p >> i);
-    }
-
-    S get(int p) const {
-        assert(0 <= p && p < _n);
-        return d[p + size];
-    }
-
-    S prod(int l, int r) const {
-        assert(0 <= l && l <= r && r <= _n);
-        S sml = e(), smr = e();
-        l += size;
-        r += size;
-
-        while (l < r) {
-            if (l & 1) sml = op(sml, d[l++]);
-            if (r & 1) smr = op(d[--r], smr);
-            l >>= 1;
-            r >>= 1;
-        }
-        return op(sml, smr);
-    }
-
-    S all_prod() const { return d[1]; }
-
-    template <bool (*f)(S)> int max_right(int l) const {
-        return max_right(l, [](S x) { return f(x); });
-    }
-    template <class F> int max_right(int l, F f) const {
-        assert(0 <= l && l <= _n);
-        assert(f(e()));
-        if (l == _n) return _n;
-        l += size;
-        S sm = e();
-        do {
-            while (l % 2 == 0) l >>= 1;
-            if (!f(op(sm, d[l]))) {
-                while (l < size) {
-                    l = (2 * l);
-                    if (f(op(sm, d[l]))) {
-                        sm = op(sm, d[l]);
-                        l++;
-                    }
-                }
-                return l - size;
-            }
-            sm = op(sm, d[l]);
-            l++;
-        } while ((l & -l) != l);
-        return _n;
-    }
-
-    template <bool (*f)(S)> int min_left(int r) const {
-        return min_left(r, [](S x) { return f(x); });
-    }
-    template <class F> int min_left(int r, F f) const {
-        assert(0 <= r && r <= _n);
-        assert(f(e()));
-        if (r == 0) return 0;
-        r += size;
-        S sm = e();
-        do {
-            r--;
-            while (r > 1 && (r % 2)) r >>= 1;
-            if (!f(op(d[r], sm))) {
-                while (r < size) {
-                    r = (2 * r + 1);
-                    if (f(op(d[r], sm))) {
-                        sm = op(d[r], sm);
-                        r--;
-                    }
-                }
-                return r + 1 - size;
-            }
-            sm = op(d[r], sm);
-        } while ((r & -r) != r);
-        return 0;
-    }
-
-  private:
-    int _n, size, log;
-    std::vector<S> d;
-    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
-};
-// l 0 base r 1 base 
-int op(int x ,int y){
-   return x+y;
-}
-int e(){
-   return 0 ;
-}
-
-
-
-
 
 struct Node{
      
