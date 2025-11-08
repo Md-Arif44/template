60680246325ac114e0ff812d5b71a5a45def3da7
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-11-08 19:56:24 +0600

    Update crt.h

--- crt.h
+++ crt.h
@@ -32,3 +32,33 @@ ll crt(ll a, ll m, ll b, ll n) {
 	x = (b - a) % n * x % n / g * m + a;
 	return x < 0 ? x + m*n/g : x;
 }
+
+/**
+ * Author: Lukas Polacek
+ * Date: 2009-09-28
+ * License: CC0
+ * Source: folklore
+ * Description: Operators for modular arithmetic. You need to set {\tt mod} to
+ * some number first and then you can use the structure.
+ */
+
+#include "euclid.h"
+
+const ll mod = 17; // change to something else
+struct Mod {
+	ll x;
+	Mod(ll xx) : x(xx) {}
+	Mod operator+(Mod b) { return Mod((x + b.x) % mod); }
+	Mod operator-(Mod b) { return Mod((x - b.x + mod) % mod); }
+	Mod operator*(Mod b) { return Mod((x * b.x) % mod); }
+	Mod operator/(Mod b) { return *this * invert(b); }
+	Mod invert(Mod a) {
+		ll x, y, g = euclid(a.x, mod, x, y);
+		assert(g == 1); return Mod((x + mod) % mod);
+	}
+	Mod operator^(ll e) {
+		if (!e) return Mod(1);
+		Mod r = *this ^ (e / 2); r = r * r;
+		return e&1 ? *this * r : r;
+	}
+};
