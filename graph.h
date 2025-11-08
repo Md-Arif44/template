3f4c3ea743c6c8fd600a5356bbd4f1e2bbd2ccd4
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-11-08 19:42:28 +0600

    Document topoSort function in graph.h
    Added documentation for the topoSort function, explaining its purpose, algorithm complexity, and behavior with cycles.

--- graph.h
+++ graph.h
@@ -29,4 +29,23 @@ vi SCC(const V<vi> &g ,V <vi>&rg){
 }
 
 
-
+/**
+ * Author: Unknown
+ * Date: 2002-09-13
+ * Source: predates tinyKACTL
+ * Description: Topological sorting. Given is an oriented graph.
+ * Output is an ordering of vertices, such that there are edges only from left to right.
+ * If there are cycles, the returned list will have size smaller than $n$ -- nodes reachable
+ * from cycles will not be returned.
+ * Time: $O(|V|+|E|)$
+ * Status: stress-tested
+ */
+ 
+vi topoSort(const vector<vi>& gr) {
+  vi indeg(sz(gr)), q;
+  for (auto& li : gr) for (int x : li) indeg[x]++;
+  rep(i,sz(gr)) if (indeg[i] == 0) q.pb(i);
+  rep(j,sz(q)) for (int x : gr[q[j]])
+    if (--indeg[x] == 0) q.pb(x);
+  return q;
+}
