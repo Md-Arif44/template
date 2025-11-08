638effe566ec5e9185f55dabda8ff17867d19ee3
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-11-08 20:24:58 +0600

    Refactor modular arithmetic structure and change mod value

--- crt.h
+++ crt.h
@@ -42,23 +42,22 @@ ll crt(ll a, ll m, ll b, ll n) {
  * some number first and then you can use the structure.
  */
 
-#include "euclid.h"
 
-const ll mod = 17; // change to something else
-struct Mod {
-	ll x;
-	Mod(ll xx) : x(xx) {}
-	Mod operator+(Mod b) { return Mod((x + b.x) % mod); }
-	Mod operator-(Mod b) { return Mod((x - b.x + mod) % mod); }
-	Mod operator*(Mod b) { return Mod((x * b.x) % mod); }
-	Mod operator/(Mod b) { return *this * invert(b); }
-	Mod invert(Mod a) {
-		ll x, y, g = euclid(a.x, mod, x, y);
-		assert(g == 1); return Mod((x + mod) % mod);
-	}
-	Mod operator^(ll e) {
-		if (!e) return Mod(1);
-		Mod r = *this ^ (e / 2); r = r * r;
-		return e&1 ? *this * r : r;
-	}
+const ll mod = 1e9+7; // change to something else
+struct Mint {
+  ll x;
+  Mint(ll xx) : x(xx) {}
+  Mint operator+(Mint b) { return Mint((x + b.x) % mod); }
+  Mint operator-(Mint b) { return Mint((x - b.x + mod) % mod); }
+  Mint operator*(Mint b) { return Mint((x * b.x) % mod); }
+  Mint operator/(Mint b) { return *this * invert(b); }
+  Mint invert(Mint a) {
+    ll x, y, g = euclid(a.x, mod, x, y);
+    assert(g == 1); return Mint((x + mod) % mod);
+  }
+  Mint power(ll e) {
+    if (!e) return Mint(1);
+    Mint r = this-> power (e / 2); r = r * r;
+    return e&1 ? *this * r : r;
+  }
 };
