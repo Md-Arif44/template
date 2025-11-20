79e68f47532ad5228cb84cf8606f6279ccc7cef2
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-11-20 11:55:23 +0600

    Refactor HLD structure and update methods

--- HLD.cpp
+++ HLD.cpp
@@ -1,57 +1,6 @@
-// ord =  ord[pos[i]] = a[i], ord for segtree   ; 
-struct HLD {
-  V<V<int>> adj ;
-  vi pa, dth, havy, head, pos;
-  int cur_pos,n;
-
-int dfs(int v ) {
-    int size = 1;
-    int max_c_size = 0;
-    for (int c : adj[v]) {
-        if (c != pa[v]) {
-            pa[c] = v, dth[c] = dth[v] + 1;
-            int c_size = dfs(c);
-            size += c_size;
-            if (c_size > max_c_size)
-                max_c_size = c_size, havy[v] = c;
-        }
-    }
- return size;
-}
-
-void decompose(int v, int h) {
-    head[v] = h, pos[v] = cur_pos++;
-    if (havy[v] != -1) decompose(havy[v], h);
-    for (int c : adj[v]) {
-        if (c != pa[v] && c != havy[v]) decompose(c, c);
-    }
-}
-
- HLD (V<V<int>> &G ): n(sz(G)){
-    adj=G; cur_pos = 0;
-    pa=vi(n),dth=vi(n),havy=vi(n,-1),head=vi(n),pos=vi(n);
-    dfs(0);  decompose(0, 0);
-}
-// path [a,b]
- V<pii> path (int a, int b) {
-  V< pii > Seg ;
-    for (; head[a] != head[b]; b = pa[head[b]]) {
-
-          if(dth[head[a]] > dth[head[b]]) swap(a, b);
-      
-      Seg.pb ({pos[head[b]], pos[b]});
-    }
-      if(dth[a] > dth[b]) swap(a, b);
-    
-      Seg.pb({pos[a], pos[b]});
-    
-    return Seg;
-  }
-} ;
-
-
 // build  ord[ Id[i] ] =  a[i] for  tree 
-template <bool VALS_EDGES = false > struct HLD {
+template <bool VALS_EDGES = false >
+struct HLD {
   int N, tim = 0;
   vector<vi> adj;
   vi par, siz, rt, pos;
@@ -94,6 +43,3 @@ template <bool VALS_EDGES = false > struct HLD {
     return  {pos[v] + VALS_EDGES, pos[v] + siz[v]} ;
   }
 };
-
-
-
