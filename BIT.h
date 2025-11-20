188a490ee83b28582762721719160816f55a89a1
Md-Arif44 <mohammadarifsb2018@gmail.com>
2025-11-20 12:17:46 +0600

    Refactor Fenwick Tree implementation in BIT.h
    Refactor BIT.h to improve functionality and clarity of the Fenwick Tree implementation. Added update, query, and lower_bound methods with necessary adjustments.

--- BIT.h
+++ BIT.h
@@ -1,16 +1,35 @@
-template <class T=ll >
+
+/*
+ * Description: \texttt{update(i,x): a[i] += x;}\\
+ * \texttt{query(i): sum in [0, i);}\\
+ * \texttt{lower\_bound(sum): min pos st sum of [0, pos] >= sum, returns n if all < sum, or -1 if empty sum.
+ */
 struct FT {
-  vector<T> x;
-  FT(int n) : x(n+1) { } 
-  void add(int k, T a) { // x[k] += a
-    for (++k; k < x.size(); k += k&-k) x[k] += a;
+  int n; V<ll> s;
+  FT(int _n) : n(_n), s(_n) {}
+  void update(int i, ll x) { 
+    for (; i < n; i |= i + 1) s[i] += x; }
+  ll query(int i, ll r = 0) { 
+    for (; i > 0; i &= i - 1) r += s[i-1]; return r; }
+  int lower_bound(ll sum) {
+    if (sum <= 0) return -1; int pos = 0;
+    for (int pw = 1 << (31 - __builtin_clz(n)) ; pw; pw >>= 1){
+      if (pos+pw <= n && s[pos + pw-1] < sum)
+        pos += pw, sum -= s[pos-1];
+    }
+    return pos;
   }
-  T sum(int k,T s=0) { // [0,k)
-    for (; k > 0; k &= k-1) s += x[k];
-    return s;
-  }
-  T rsum(int l,int r ) { // [l,r) 
-    return sum(r) - sum(l) ;}
+}; 
+FT f1(n), f2(n);
+// a[l...r] += v; 0 <= l <= r < n
+auto upd = [&](int l, int r, ll v) {
+  f1.update(l, v), f1.update(r + 1, -v);
+  f2.update(l, v*(l-1)), f2.update(r+1, -v*r);
+}; // a[l] + ... + a[r]; 0 <= l <= r < n
+auto sum = [&](int l, int r) { ++r;
+  ll sub = f1.query(l) * (l-1) - f2.query(l);
+  ll add = f1.query(r) * (r-1) - f2.query(r);
+  return add - sub;
 };
 
 
